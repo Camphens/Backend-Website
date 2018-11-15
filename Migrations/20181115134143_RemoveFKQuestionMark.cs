ed17ba21518bd858bfd51efe55494cb180d1c855
Hayder1999 <hayderali1999@gmail.com>
2018-11-15 14:42:04 +0100

    New migrations to accomodate the change

---
+++ Migrations/20181115134143_RemoveFKQuestionMark.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendWebsite.Migrations
+{
+    public partial class RemoveFKQuestionMark : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
