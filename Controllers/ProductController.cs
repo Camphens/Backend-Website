4b6a062b4f69b05872cdbe9502c573ec1487da0f
Fiona <0957639@hr.nl>
2018-12-11 02:00:01 +0100

    Partial filtering

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Backend_Website.Models;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Linq.Expressions;
 
 namespace Backend_Website.Controllers
 {
@@ -42,13 +44,21 @@ namespace Backend_Website.Controllers
             public IOrderedQueryable products {get;set;}
         }
 
-        public class Filter
-        {
-            public string filter1 {get;set;}
-            public string filter2 {get;set;}
-            public string filter3 {get;set;}
-            public string filter4 {get;set;}
-            public string filter5 {get;set;}
+        // public class FilterV
+        // {
+        //     public string filter1 {get;set;}
+        //     public string filter2 {get;set;}
+        //     public string filter3 {get;set;}
+        //     public string filter4 {get;set;}
+        //     public string filter5 {get;set;}
+        // }
+
+        public class Filter{
+            public int kind {get;set;}
+            public string att{get;set;}
+            public object value{get;set;}
+            public Filter a1 {get;set;}
+            public Filter a2 {get;set;}
         }
 
         // GET api/product
@@ -228,39 +238,155 @@ namespace Backend_Website.Controllers
             PaginationPage page = new PaginationPage {totalpages = totalpages, totalitems = totalitems, products = res};
             return Ok(page);
         }
+
+//         [HttpGet("filter")]
+// public IActionResult GetFilter([FromBody] Filter f_p)
+// {
+//     if(f_p == null){
+//         return NotFound();
+//     }
+//     Expression<Func<Product,bool>> e_p = FilterToExpr<Product>(f_p);
+//     return Ok(_context.Products.Where(e_p).ToArray());
+// }
+
+// Expression<Func<T,bool>> FilterToExpr<T>(Filter f){
+//     var type = typeof(T);
+//     var parameter = Expression.Parameter(type, "P");
+//     return FilterToExpr_AUX<T>(f, parameter);
+// }
+// Expression<Func<T,bool>> FilterToExpr_AUX<T>(Filter f, ParameterExpression parameter){
+//     switch(f.kind){
+//         case 0:{
+//             var propertyReference = Expression.Property(parameter, f.att);
+//             var constantReference = Expression.Constant(f.value);
+//             return Expression.Lambda<Func<T,bool>>(Expression.Equal(propertyReference, constantReference), parameter);
+//         }
+//         case 1:{
+//             Expression<Func<T,bool>> expr1 = FilterToExpr_AUX<T>(f.a1, parameter);
+//             Expression<Func<T,bool>> expr2 = FilterToExpr_AUX<T>(f.a2, parameter);
+//             var body = Expression.And(expr1.Body, expr2.Body);
+//             var Lambda = Expression.Lambda<Func<T,bool>>(body, parameter);
+//             return Lambda;
+//         }
+//         default: return null;
+//     }
+// }
+// }
         
 
-        [HttpGet("filter/{page_index}/{page_size}")]
-        public IActionResult Filtering(int page_index, int page_size, dynamic filter)
-        {
-            dynamic FilterString = JsonConvert.DeserializeObject(filter.ToString());
+        // [HttpGet("filter/{page_index}/{page_size}")]
+        // public IActionResult Filtering(int page_index, int page_size, dynamic filter)
+        // {
+        //     dynamic FilterString = JsonConvert.DeserializeObject(filter.ToString());
             
-            Filter filterclass = new Filter{filter1 = FilterString.filter1, filter2 = FilterString.filter2, filter3 = FilterString.filter3, filter4 = FilterString.filter4, filter5 = FilterString.filter5};
-            var filter_brandname = (from b in _context.Brands where b.BrandName == filterclass.filter1 select b);
-            var filter_collectiename = (from col in _context.Collections where col.CollectionName == filterclass.filter2 select col);
-            var filter_type = (from type in _context.Types where type._TypeName == filterclass.filter3 select type);
-            var filter_category = (from cat in _context.Categories where cat.CategoryName == filterclass.filter4 select cat); 
-            var res = (from p in _context.Products from bn in filter_brandname from cn in filter_collectiename from tn in filter_type from catn in filter_category where (p._TypeId == tn.Id) && (p.CategoryId == catn.Id) && (p.BrandId == bn.Id) && (p.CollectionId == cn.Id) && (p.ProductColor == filterclass.filter5) orderby p.Id let images = 
-            (from i in _context.ProductImages where p.Id == i.ProductId select i.ImageURL).ToArray()
-            let type = (from t in _context.Types where p._TypeId == t.Id select t._TypeName)
-            let category = (from cat in _context.Categories where p.CategoryId == cat.Id select cat.CategoryName)
-            let collection = (from c in _context.Collections where p.CollectionId == c.Id select c.CollectionName)
-            let brand = (from b in _context.Brands where p.BrandId == b.Id select b.BrandName)
-            let stock = (from s in _context.Stock where p.StockId == s.Id select s.ProductQuantity)
-            select new Complete_Product(){Product = p, Images = images, Type = type, Category = category, Collection = collection, Brand = brand, Stock = stock}).ToArray();
+        //     Filter filterclass = new Filter{filter1 = FilterString.filter1, filter2 = FilterString.filter2, filter3 = FilterString.filter3, filter4 = FilterString.filter4, filter5 = FilterString.filter5};
+        //     var filter_brandname = (from b in _context.Brands where b.BrandName == filterclass.filter1 select b);
+        //     var filter_collectiename = (from col in _context.Collections where col.CollectionName == filterclass.filter2 select col);
+        //     var filter_type = (from type in _context.Types where type._TypeName == filterclass.filter3 select type);
+        //     var filter_category = (from cat in _context.Categories where cat.CategoryName == filterclass.filter4 select cat); 
+        //     var res = (from p in _context.Products from bn in filter_brandname from cn in filter_collectiename from tn in filter_type from catn in filter_category where (p._TypeId == tn.Id) && (p.CategoryId == catn.Id) && (p.BrandId == bn.Id) && (p.CollectionId == cn.Id) && (p.ProductColor == filterclass.filter5) orderby p.Id let images = 
+        //     (from i in _context.ProductImages where p.Id == i.ProductId select i.ImageURL).ToArray()
+        //     let type = (from t in _context.Types where p._TypeId == t.Id select t._TypeName)
+        //     let category = (from cat in _context.Categories where p.CategoryId == cat.Id select cat.CategoryName)
+        //     let collection = (from c in _context.Collections where p.CollectionId == c.Id select c.CollectionName)
+        //     let brand = (from b in _context.Brands where p.BrandId == b.Id select b.BrandName)
+        //     let stock = (from s in _context.Stock where p.StockId == s.Id select s.ProductQuantity)
+        //     select new Complete_Product(){Product = p, Images = images, Type = type, Category = category, Collection = collection, Brand = brand, Stock = stock}).ToArray();
 
-            int totalitems = res.Count();
-            int totalpages = totalitems / page_size;
-            //totalpages+1 because the first page is 1 and not 0
-            totalpages = totalpages+1;
-            string Error = "Error";
-            if (res.Count() < 1 | page_index < 1) return Ok(Error);
-            //page_index-1 so the first page is 1 and not 0
-            page_index = page_index-1;
-            int skip = page_index * page_size;
-            res = res.Skip(skip).Take(page_size).ToArray();
-            PaginationPage page = new PaginationPage {totalpages = totalpages, totalitems = totalitems, products = res};
-            return Ok(page);
+        //     int totalitems = res.Count();
+        //     int totalpages = totalitems / page_size;
+        //     //totalpages+1 because the first page is 1 and not 0
+        //     totalpages = totalpages+1;
+        //     string Error = "Error";
+        //     if (res.Count() < 1 | page_index < 1) return Ok(Error);
+        //     //page_index-1 so the first page is 1 and not 0
+        //     page_index = page_index-1;
+        //     int skip = page_index * page_size;
+        //     res = res.Skip(skip).Take(page_size).ToArray();
+        //     PaginationPage page = new PaginationPage {totalpages = totalpages, totalitems = totalitems, products = res};
+        //     return Ok(page);
+        // }
+        
+        [HttpGet("filter")]
+               public IActionResult Filters(Dictionary<string,string> filters)
+        {
+            //  foreach (var item in filterarray){
+            //     OrderProduct product = new OrderProduct(){
+            //         OrderId = Order.Id, 
+            //         ProductId = item};
+            //     _context.OrderProduct.Add(product);}
+                             
+            // Console.WriteLine($"HERE {filters}");
+
+        //      foreach( KeyValuePair<string, string> kvp in filters)
+        // {
+        //     Console.WriteLine("HERE Key = {0}, Value = {1}", 
+        //         kvp.Key, kvp.Value);
+            
+        // }
+
+        // IOrderedQueryable x = null;
+       Object[] x= new Object[]{};
+        //IQueryable<int> x = null;
+        //Product[] x = new Product[]{};
+                List<object> List = new List<object>{};
+        List<object> Values = new List<object>{};
+        List<string> Keys = new List<string>{};
+        
+        foreach (KeyValuePair<string,string> item in filters)
+        {
+            
+            Values.Add(item.Value);
+            Keys.Add(item.Key);
+            Console.WriteLine($"HERE {item.Key} & {item.Value}");
+           x = (from p in _context.Products.Where(f => f.GetProperty(item.Key) == item.Value) select p).ToArray();
+            List.Add(x);
+            //return Ok(x);
         }
+// foreach (string key in Keys){
+//     foreach (object value in Values){
+//               x = (from p in _context.Products.Where(f => f.GetProperty(key) == value) select p);  
+//               Console.WriteLine($"HERE {key} & {value}");}
+//             }
+        // foreach (object item in Keys)
+        // {
+           
+        // }
+        
+        // foreach (KeyValuePair<string,object> item in filters)
+        // {
+        // //    if (item.Key == "BrandId"){
+        // //      var res = from p in _context.Products where p.BrandId == item.Value orderby p select p.BrandId;
+
+        //     //  var x = res.Where(f => f.GetProperty("BrandId") == item.Value);
+        //     //  List.Add(res);}
+              
+        //     //  var res = from p in _context.Products where p.CollectionId == item.Value orderby p select p;
+        //    x = (from p in _context.Products.Where(f => f.GetProperty("BrandId") == item.Value) select p).ToArray();
+        //     //  var x = res.Where(f => f.GetProperty("BrandId") == item.Value);
+        //      List.Add(x);
+
+
+          
+        // }
+         //var filter = filters.Keys;
+        //   Console.WriteLine($"HERE {filters.Keys}");
+       // Console.WriteLine($"HERE {x}");
+            return Ok(x);
+
+            
+        }
+
+        
     }
+  public static class MyExtensions
+{
+    public static string GetProperty<T>(this T obj, string name)
+    {
+        Type t = typeof(T);
+        return t.GetProperty(name).GetValue(obj, null).ToString();
+    } 
+ 
+}
+    
 } 
