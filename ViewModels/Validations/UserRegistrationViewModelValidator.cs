0f4421ab7f4aa306cb8a7f99ff5b95ead9b20598
Camphens <37102286+Camphens@users.noreply.github.com>
2019-01-13 14:37:37 +0100

    Update UserRegistrationViewModelValidator.cs

--- ViewModels/Validations/UserRegistrationViewModelValidator.cs
+++ ViewModels/Validations/UserRegistrationViewModelValidator.cs
@@ -8,7 +8,8 @@ namespace Backend_Website.ViewModels.Validations
     {
         public UserRegistrationViewModelValidator()
         {
-            RuleFor(a => a.BirthDate).GreaterThanOrEqualTo(new System.DateTime(1860, 1, 1, 0, 0, 0)).LessThan(System.DateTime.Now).WithMessage("Geboortedatum onjuist");
+            RuleFor(a => a.BirthDate).GreaterThanOrEqualTo(new System.DateTime(1860, 1, 1, 0, 0, 0)).WithMessage("Geboortedatum onjuist");
+            RuleFor(a => a.BirthDate).LessThanOrEqualTo(new System.DateTime(2019, 1, 1, 0, 0, 0)).WithMessage("Geboortedatum onjuist");
             //RuleFor(a => a.EmailAddress)..WithMessage("o");
             RuleFor(a => a.UserPassword).MinimumLength(3).WithMessage("Wachtwoord moet meer dan 3 tekens bevatten");
         }
